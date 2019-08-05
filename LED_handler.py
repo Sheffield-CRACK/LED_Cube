3b10dc4c8dd5d8773c72e4c56139e1143fcdc473
wildjames <jwild2@sheffield.ac.uk>
2019-08-05 20:49:00 +0100

    Threaded the LED handler

--- LED_handler.py
+++ LED_handler.py
@@ -3,19 +3,27 @@
 Anode rails must be on
 '''
 
-import gpiozero
+import threading
 import time
+
+import gpiozero
 import numpy as np
+
 
 class LEDHandler(object):
     '''Control the GPIO Pins to operate the LED cube.'''
-    def __init__(self, grid, pulse_duration=20e-3):
-        self.X, self.Y, self.Z = grid.shape
+    def __init__(self, grid, pulse_duration=5e-3, duty_cycle=0.5, debug=False):
+        self.debug = debug
         self.grid = grid
+
+        if self.debug:
+            print("Grid initialised to:")
+            print(self.grid)
 
         # TODO:
         # Should this be a grid flash frequency, rather than a pulse duration?
         self.pulse_duration = pulse_duration
+        self.duty_cycle = duty_cycle
 
         # The cathodes are assumed to be the first dimension.
         # These will control transistors that let the current flow through
@@ -26,7 +34,8 @@ class LEDHandler(object):
             for i in range(2, self.X+2)
         ]
 
-        # There will be Y*Z anodes. They will control whether an index is on or off.
+        # There will be Y*Z anodes.
+        # They will control whether an index is on or off.
         start = self.X + 2
         stop = start + (self.Y * self.Z)
         self.cathodes = [gpiozero.DigitalOutputDevice(
@@ -34,44 +43,94 @@ class LEDHandler(object):
             for j in range(start, stop)
         ]
 
+        # Start a thread that literally just calls the flash_grid method
+        # over and over, ad infinitum.
+        self.is_on = False
+        self.thread = threading.Thread(target=self.__run__)
+
+    def __run__(self):
+        while True:
+            sleep = (1.0-self.duty_cycle) / self.pulse_frequency
+            time.sleep(sleep)
+            self.flash_grid()
+
+    @property
+    def is_on(self):
+        return self.__is_on
+    @is_on.setter
+    def is_on(self, is_on):
+        self.__is_on = is_on
+
+    @property
+    def grid(self):
+        return self.__grid
+    @grid.setter
+    def grid(self, grid):
+        self.__grid = grid
+
+        # Save the dimensions
+        self.X, self.Y, self.Z = grid.shape
+
+    @property
+    def pulse_frequency(self):
+        n_flashes = float(self.X + (self.Y * self.Z))
+        time_per_flash = self.pulse_duration * n_flashes
+
+        return 1./time_per_flash
 
     def flash_grid(self):
-        for cathode, layer in zip(self.cathodes, self.grid):
-            print("Activating {}".format(cathode.pin))
-            cathode.on()
+        '''If I'm on, flash the grid. Otherwise, just do nothing. '''
+        if self.is_on:
+            for cathode, layer in zip(self.cathodes, self.grid):
+                if self.debug:
+                    print("Activating {}".format(cathode.pin))
+                cathode.on()
 
-            # Get the layer in 1D - the LEDs are hooked up in only 2D,
-            # but arranged in 3D!
-            layer = layer.flatten()
+                # Get the layer in 1D - the LEDs are hooked up in only 2D,
+                # but arranged in 3D!
+                layer = layer.flatten()
 
-            # Activate the pins we want
-            for anode, is_on in zip(self.anodes, layer):
-                if is_on:
-                    print("Activating pin {}".format(anode.pin))
-                    anode.on()
+                # Activate the pins we want
+                for anode, is_on in zip(self.anodes, layer):
+                    if is_on:
+                        if self.debug:
+                            print("Activating pin {}".format(anode.pin))
+                        anode.on()
 
-            # Leave them on for long enough to see
-            time.sleep(self.pulse_duration)
-            for anode in self.anodes:
-                anode.off()
-            cathode.off()
+                # Leave them on for long enough to see
+                time.sleep(self.pulse_duration)
+                if self.debug:
+                    print("Resetting all pins")
+                for anode in self.anodes:
+                    anode.off()
+                cathode.off()
+
+        # If I'm not on, cycle through just the same, but without toggling the
+        # pins
+        else:
+            if self.debug:
+                print("LED grid not active")
+            for cathode in self.cathodes:
+                for anode in self.anodes:
+                    time.sleep(self.pulse_duration)
+
 
     def flash_LED(self, x, y, z, pulse_duration=1):
         '''Flash the LED at coordinate (x, y, z)'''
         # LAYER 1
-        # [[[0, 0, 0],
+        # [[[1, 0, 0],
         #   [0, 0, 0],
         #   [0, 0, 0]],
 
         # LAYER 2
         #  [[0, 0, 0],
-        #   [0, 0, 0],
+        #   [0, 1, 0],
         #   [0, 0, 0]],
 
         # LAYER 3
         #  [[0, 0, 0],
         #   [0, 0, 0],
-        #   [0, 0, 0]]]
+        #   [0, 0, 1]]]
 
         # The anode we want is easy - just the layer.
         self.anodes[x].on()
