21cedf8d1b7f0b68dee25d09b87d27ab6af46728
wildjames <jwild2@sheffield.ac.uk>
2019-11-19 18:06:57 +0000

    Slow down

--- shift_register.py
+++ shift_register.py
@@ -1,23 +1,26 @@
 import gpiozero
 import time
 
+DELAY = 5
+
 data = gpiozero.DigitalOutputDevice(17, initial_value=False)
 clock = gpiozero.DigitalOutputDevice(18, initial_value=False)
 
+print("Okay, going")
 
 data.off()
 
 clock.on()
-time.sleep(0.100) # 100ms
+time.sleep(DELAY) # 100ms
 clock.off()
-time.sleep(0.100)
+time.sleep(DELAY)
 
 data.on()
 
 clock.on()
-time.sleep(0.100) # 100ms
+time.sleep(DELAY) # 100ms
 clock.off()
-time.sleep(0.100)
+time.sleep(DELAY)
 
 
 
