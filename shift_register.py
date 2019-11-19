9e83340948c7d63d0257a2d9aaaacebb8b57439e
wildjames <jwild2@sheffield.ac.uk>
2019-11-19 18:13:38 +0000

    DELETEME

--- shift_register.py
+++ shift_register.py
@@ -6,58 +6,26 @@ DELAY = 5
 
 
 data = gpiozero.DigitalOutputDevice(17, initial_value=False)
-clock = gpiozero.DigitalOutputDevice(18, initial_value=False)
+clock = gpiozero.DigitalOutputDevice(27, initial_value=False)
 
 print("Okay, going")
 
-data.off()
-print("Data off, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Sent!")
-time.sleep(DELAY)
+for _ in range(8):
+    data.off()
+    print("Data off, sending clock pulse")
+    clock.on()
+    time.sleep(PULSE_WIDTH) # 100ms
+    clock.off()
+    print("Sent!")
+    time.sleep(DELAY)
 
-data.on()
-print("Data on, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Done!")
-time.sleep(DELAY)
-
-data.off()
-print("Data off, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Sent!")
-time.sleep(DELAY)
-
-data.on()
-print("Data on, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Done!")
-time.sleep(DELAY)
-
-data.off()
-print("Data off, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Sent!")
-time.sleep(DELAY)
-
-data.on()
-print("Data on, sending clock pulse")
-clock.on()
-time.sleep(PULSE_WIDTH) # 100ms
-clock.off()
-print("Done!")
-time.sleep(DELAY)
-
+    data.on()
+    print("Data on, sending clock pulse")
+    clock.on()
+    time.sleep(PULSE_WIDTH) # 100ms
+    clock.off()
+    print("Done!")
+    time.sleep(DELAY)
 
 
 print("Setting data and clock both to off")
