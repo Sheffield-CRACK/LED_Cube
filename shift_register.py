16c6d59e8fa53f2fcedf432152476b1cfd047adf
wildjames <jwild2@sheffield.ac.uk>
2019-11-19 18:34:56 +0000

    DELETEME

--- shift_register.py
+++ shift_register.py
@@ -9,31 +9,31 @@ data = gpiozero.DigitalOutputDevice(17, initial_value=False, active_high=True)
 clock = gpiozero.DigitalOutputDevice(27, initial_value=False, active_high=True)
 reset = gpiozero.DigitalOutputDevice(22, initial_value=True, active_high=True)
 
+def clockpulse():
+    clock.on()
+    time.sleep(PULSE_WIDTH)
+    clock.off()
+
 
 print("Okay, going")
 
 print("Sending reset pulse:")
 reset.off()
-clock.on()
-time.sleep(DELAY)
-clock.off()
+clockpulse()
+reset.on()
 
 print("Sending data...")
 
 for _ in range(8):
     data.off()
     print("Data off, sending clock pulse")
-    clock.on()
-    time.sleep(PULSE_WIDTH) # 100ms
-    clock.off()
+    clockpulse()
     print("Sent!")
     time.sleep(DELAY)
 
     data.on()
     print("Data on, sending clock pulse")
-    clock.on()
-    time.sleep(PULSE_WIDTH) # 100ms
-    clock.off()
+    clockpulse()
     print("Sent!")
     time.sleep(DELAY)
 
@@ -41,3 +41,4 @@ for _ in range(8):
 print("Setting data and clock both to off")
 data.off()
 clock.off()
+reset.on()
