14ca36d08f3f99b1948b6ece54f79b84e3cd6028
wildjames <jwild2@sheffield.ac.uk>
2019-11-20 17:33:55 +0000

    DELETEME

--- shift_register.py
+++ shift_register.py
@@ -5,13 +5,9 @@ PULSE_WIDTH = 300/1000
 DELAY = 5
 
 
-# data = gpiozero.DigitalOutputDevice(17, initial_value=False, active_high=True)
-# clock = gpiozero.DigitalOutputDevice(27, initial_value=False, active_high=True)
-# reset = gpiozero.DigitalOutputDevice(22, initial_value=True, active_high=True)
-
-data = gpiozero.DigitalOutputDevice(11, initial_value=False, active_high=True)
-clock = gpiozero.DigitalOutputDevice(13, initial_value=False, active_high=True)
-reset = gpiozero.DigitalOutputDevice(15, initial_value=True, active_high=True)
+data = gpiozero.DigitalOutputDevice(17, initial_value=False, active_high=True)
+clock = gpiozero.DigitalOutputDevice(27, initial_value=False, active_high=True)
+reset = gpiozero.DigitalOutputDevice(22, initial_value=True, active_high=True)
 
 def clockpulse():
     clock.on()
