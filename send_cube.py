d333143dba35cdaa68328889869c6672fffba0c7
wildjames <jwild2@sheffield.ac.uk>
2020-01-21 22:03:13 +0000

    Tried to make the connect_cube detect the arduino automagically

--- send_cube.py
+++ send_cube.py
@@ -1,12 +1,35 @@
+'''James Wild, Jan. 2020.
+
+LED cube matrix python handler. The Arduino does the boring grunt work of
+sorting the cube out, so that you can generate cool stuff here without worrying
+too much about nonsense.
+
+As far as I've tested so far, the transmit time is pretty negligible (I think
+on the order 1ms, but fuck knows), so if you want people to see whatever you're
+throwing up there you will likely have to add in delays.
+
+Most of this is stuff YOU don't need to care about. For the end-user, it is simple:
+    arduino = connect_cube() # Searches ports for a connected arduino
+
+    data = np.ones((8,8,8))  # Fully illuminates the cube
+    send_cube(arduino, data) # Send the data
+
+    data = np.zeros((8,8,8)) # Turns the cube off
+    send_cube(arduino, data) # Send the data
+
+    arduino.close()          # Close the connection to the arduino.
+'''
+
 import serial
 import numpy as np
 import time
-from pprint import pprint
-import binascii
+import serial.tools.list_ports
 
 N_REGISTERS = 8 # How many registers are daisy-chained?
 N_BITS      = 8 # How many bits does each SR handle? (usulaly 8)
 N_LAYERS    = 8 # How many layers do we have?
+
+BAUDRATE = 9600
 
 
 def conv2byte(byte):
@@ -52,38 +75,42 @@ def conv2byte_vect(cube):
     new_cube = bytearray(new_cube)
     return new_cube
 
-def connect_cube(dev_path):
-    '''Connects to the cube.
+def connect_cube():
+    '''Connects to the cube. Searches the connected ports for something with
+    "Arduino" in its description, then connects to it.
 
     USAGE:
+        arduino = connect_cube()
         cube = np.ones(8,8,8)
-        ser = connect_cube(dev_path)
-        send_cube(ser, cube)
-
-    To find the dev_path:
-        Find Port Number on Macintosh
-            Open terminal and type: ls /dev/*.
-            Note the port number listed for /dev/tty.usbmodem* or /dev/tty.usbserial*. The port number is represented with * here.
-
-        Find Port Number on Linux
-            Open terminal and type: ls /dev/tty*.
-            Note the port number listed for /dev/ttyUSB* or /dev/ttyACM*. The port number is represented with * here.
-
-        Find Port Number on Windows
-            Open Device Manager, and expand the Ports (COM & LPT) list.
-            Note the number on the USB Serial Port.
-
-        Use the listed port as the serial port in MATLAB®. For example: /dev/ttyUSB0.
-
-    Or, just google it. Use your initiative, since I haven't found a consistent way of doing this >:(
+        send_cube(arduino, cube)
     '''
-    ser = serial.Serial(dev_path, 9600, timeout=.1)
+    print("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*")
+    print("| SEARCHING FOR AN ARDUINO... |")
+    print("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*")
+
+    ports = list(serial.tools.list_ports.comports())
+    Arduino_ports=[]
+    for p in ports:
+        if 'Arduino' in p.description:
+            Arduino_ports.append(p)
+
+    if len(Arduino_ports) == 0:
+        raise Exception("no Arduino board detected")
+
+    if len(Arduino_ports) > 1:
+        print('Multiple Arduinos found - using the first')
+    else:
+        print("Arduino board detected")
+
+    if len(Arduino_ports) >= 1:
+        print(Arduino_ports[0].device)
+        arduino = serial.Serial(Arduino_ports[0].device, BAUDRATE, timeout=.1)
 
     print("Waiting for Arduino to start...")
     time.sleep(2)
     print("\n\n\nReady to start transmitting!")
 
-    return ser
+    return arduino
 
 def send_cube(ser, cube):
     '''Send a 3D array to the cube, down the serial pipe.
@@ -110,7 +137,7 @@ if __name__ in "__main__":
 
 
     # Start the serial connection
-    ser = connect_cube('/dev/cu.wchusbserial72')
+    ser = connect_cube()
     send_cube(data)
     time.sleep(2)
 
